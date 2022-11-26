da8d476f10633ab68fbe4a16d1f2f10eb7a37b5d
vitun7komee <118392333+vitun7komee@users.noreply.github.com>
2022-11-26 22:32:53 +0300

    Add files via upload

---
+++ kapl2.cpp
@@ -0,0 +1,45 @@
+﻿#include "My_List.h"
+#include <Windows.h>
+
+void task(DLIST& DL, std::string searchname,int targetyear)
+{
+	ptrNODE ptr = DL.get_begin();
+	bool stop = true;
+	std::cout << "\n\nSearch results : \n\n";
+	std::cout << "~~~~~~~~~~~~~~~~~~\n";
+	while (ptr) {
+		if ((ptr->info.getyear() > targetyear || ptr->info.getyear() == targetyear) && ptr->info.search(searchname, ptr->info.getname())) {
+			ptr->info.print();
+		}
+		else { break; }
+		ptr = ptr->next;
+	}
+	ptr = DL.get_begin();
+	while (stop) {
+		if ((ptr->info.getyear() > targetyear || ptr->info.getyear() == targetyear) && ptr->info.search(searchname, ptr->info.getname())) {
+			ptr->info.print();
+		}
+		else { stop = false; }
+		ptr = ptr->next;
+	}
+}
+
+int main()
+{
+	setlocale(0, "");
+	std::ifstream file("xdd.txt");
+	if (file)
+	{
+		DLIST DL("xdd.txt");
+		DL.print();
+		std::string searchname;
+		int targetyear;
+		std::cout << "\n\nEnter name of the book: ";
+		std::cin >> searchname;
+		std::cout << "Enter the year of publishing: ";
+		std::cin >> targetyear;
+		task(DL, searchname, targetyear);
+	}
+	else { std::cout << "Input file not found" << std::endl; }
+	std::cin.get();
+}
